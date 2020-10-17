6d611497dde7ed8585caf60fd3c99c8568973e6b
Álvaro González <alvaro@code.cl>
2020-10-17 11:03:47 -0300

    NOTASK Removed bundler analyzer. Added .env.example. Code normalization

--- webpack.config.js
+++ webpack.config.js
@@ -3,13 +3,6 @@ const commonConfig = require('./build-utils/webpack.common');
 
 const webpackMerge = require('webpack-merge');
 
-const addons = (/* string | string[] */ addonsArg) => {
-  let addons = [...[addonsArg]] // Normalize array of addons (flatten)
-    .filter(Boolean); // If addons is undefined, filter it out
-
-  return addons.map(addonName => require(`./build-utils/addons/webpack.${addonName}.js`));
-};
-
 module.exports = env => {
   if (!env) {
     throw new Error(buildValidations.ERR_NO_ENV_FLAG);
@@ -19,7 +12,6 @@ module.exports = env => {
   const mergedConfig = webpackMerge(
     commonConfig,
     envConfig,
-    ...addons(env.addons)
   );
 
   return mergedConfig;
